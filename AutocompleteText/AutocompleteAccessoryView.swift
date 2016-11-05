3b552dff2838c7b95c89933a6ad5ab148921986e
Ray Fix <rayfix@gmail.com>
2016-11-05 12:18:26 -0700

    upgrade to swift 3

--- AutocompleteText/AutocompleteAccessoryView.swift
+++ AutocompleteText/AutocompleteAccessoryView.swift
@@ -19,8 +19,8 @@ private class AutocompleteCell: UICollectionViewCell {
   static let padding: CGFloat = 10
   static let buttonHeight: CGFloat = 30
   static let cornerRadius: CGFloat = 4
-  static let font = UIFont.systemFontOfSize(16)
-  static let backgroundColor = UIColor(white: 0.95, alpha: 1)
+  static let font = UIFont.systemFont(ofSize: 16)
+  static let boxColor = UIColor(white: 0.95, alpha: 1)
   static let textColor = UIColor(white: 0.3, alpha: 1)
   
   var label: UILabel?
@@ -31,19 +31,19 @@ private class AutocompleteCell: UICollectionViewCell {
     }
   }
   
-  func configure(text text: String) {
+  func configure(text: String) {
     self.text = text
   }
   
   func commonInit() {
-    backgroundColor = AutocompleteCell.backgroundColor
+    backgroundColor = AutocompleteCell.boxColor
     layer.cornerRadius = AutocompleteCell.cornerRadius
     clipsToBounds = true
     let labelFrame = UIEdgeInsetsInsetRect(bounds, UIEdgeInsets(top: 0, left: AutocompleteCell.padding, bottom: 0, right: AutocompleteCell.padding))
     let label = UILabel(frame: labelFrame)
-    label.autoresizingMask = [.FlexibleWidth]
+    label.autoresizingMask = [.flexibleWidth]
     label.adjustsFontSizeToFitWidth = false
-    label.lineBreakMode = .ByClipping
+    label.lineBreakMode = .byClipping
     label.font = AutocompleteCell.font
     label.textColor = AutocompleteCell.textColor
     addSubview(label)
@@ -60,12 +60,12 @@ private class AutocompleteCell: UICollectionViewCell {
     commonInit()
   }
   
-  private override func intrinsicContentSize() -> CGSize {
+  fileprivate override var intrinsicContentSize : CGSize {
     return AutocompleteCell.size(text: text)
   }
   
-  static func size(text text: String) -> CGSize {
-    let textWidth = text.sizeWithAttributes([NSFontAttributeName: AutocompleteCell.font]).width
+  static func size(text: String) -> CGSize {
+    let textWidth = text.size(attributes: [NSFontAttributeName: AutocompleteCell.font]).width
     let itemSize = CGSize(width: textWidth + 2*AutocompleteCell.padding, height: AutocompleteCell.buttonHeight)
     return itemSize
   }
@@ -75,7 +75,7 @@ class AutocompleteAccessoryView: UIView {
   var dataSource: AutocompleteDataSource?
   var tapHandler: ((String)->Void)?
   
-  init(dataSource: AutocompleteDataSource, tapHandler:(String)->Void) {
+  init(dataSource: AutocompleteDataSource, tapHandler:@escaping (String)->Void) {
     self.dataSource = dataSource
     self.tapHandler = tapHandler
     super.init(frame:CGRect(x: 0, y: 0, width: 0, height: 44))
@@ -84,17 +84,17 @@ class AutocompleteAccessoryView: UIView {
   
   func commonInit() {
     let flowLayout = UICollectionViewFlowLayout()
-    flowLayout.scrollDirection = .Horizontal
+    flowLayout.scrollDirection = .horizontal
     flowLayout.minimumLineSpacing = 10
     flowLayout.minimumInteritemSpacing = 10
     
     let collectionView = UICollectionView(frame: bounds, collectionViewLayout: flowLayout)
-    collectionView.backgroundColor = .whiteColor()
-    collectionView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+    collectionView.backgroundColor = .white
+    collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     collectionView.allowsMultipleSelection = false
     collectionView.showsHorizontalScrollIndicator = false
     collectionView.showsVerticalScrollIndicator = false
-    collectionView.registerClass(AutocompleteCell.self, forCellWithReuseIdentifier: AutocompleteCell.reuseID)
+    collectionView.register(AutocompleteCell.self, forCellWithReuseIdentifier: AutocompleteCell.reuseID)
     collectionView.dataSource = self
     collectionView.delegate = self
     addSubview(collectionView)
@@ -106,7 +106,7 @@ class AutocompleteAccessoryView: UIView {
     commonInit()
   }
   
-  func autocompleteText(at indexPath: NSIndexPath) -> String {
+  func autocompleteText(at indexPath: IndexPath) -> String {
     return dataSource!.autoCompleteText(at: indexPath.row)
   }
   
@@ -114,32 +114,32 @@ class AutocompleteAccessoryView: UIView {
 
 extension AutocompleteAccessoryView: UICollectionViewDelegateFlowLayout {
   
-  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     let text = autocompleteText(at: indexPath)
     return AutocompleteCell.size(text: text)
   }
   
-  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
     return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
   }
 }
 
 extension AutocompleteAccessoryView: UICollectionViewDataSource {
   
-  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return dataSource?.autocompleteTextCount ?? 0
   }
   
-  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let text = autocompleteText(at: indexPath)
-    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(AutocompleteCell.reuseID, forIndexPath: indexPath) as! AutocompleteCell
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AutocompleteCell.reuseID, for: indexPath) as! AutocompleteCell
     cell.configure(text: text)
     return cell
   }
 }
 
 extension AutocompleteAccessoryView: UICollectionViewDelegate {
-  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     let text = autocompleteText(at: indexPath)
     tapHandler?(text)
   }
